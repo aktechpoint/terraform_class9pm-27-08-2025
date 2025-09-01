a4cbe34aca5b5dd94abbc615e63b1d9a2c7e266c
abhishek kumar <aktechuniverse@gmail.com>
2025-09-01 22:09:05 +0530

    hello-2

--- day-5/main.tf
+++ day-5/main.tf
@@ -2,5 +2,8 @@ resource "aws_instance" "abhishek" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+    tags = {
+        Name = "Abhishek-Instance"
+    }
   
 }
