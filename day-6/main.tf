03ab5c2ef2e0183dd8bee2d1e0b4ce819ff82fb3
abhishek kumar <aktechuniverse@gmail.com>
2025-09-02 21:26:13 +0530

    all day-6

---
+++ day-6/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "abhishek" {
+    ami = "ami-02d26659fd82cf299"
+    instance_type = "t3.micro"
+    key_name = "sun"
+    tags = {
+      Name = "abhishek-instance"
+    }
+  
+}
