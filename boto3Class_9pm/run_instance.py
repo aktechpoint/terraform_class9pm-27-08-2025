1df6057df073077a830635305ff8fe639fe1babf
abhishek kumar <aktechuniverse@gmail.com>
2025-09-20 18:04:46 +0530

    all code grafana prometheus and worker nodes

--- boto3Class_9pm/run_instance.py
+++ boto3Class_9pm/run_instance.py
@@ -3,7 +3,7 @@ import boto3
 ec2 = boto3.client('ec2')
 
 response = ec2.run_instances(
-    ImageId='ami-02d26659fd82cf299',  # Replace with a valid AMI ID
+    ImageId='ami-01b6d88af12965bb6',  # Replace with a valid AMI ID
     InstanceType='t3.micro',
     KeyName='key_12',  # Corrected parameter name (case-sensitive)
     MinCount=1,
